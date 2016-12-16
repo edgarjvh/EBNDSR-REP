aa3f2a1f92fbfb11e74012892be300ff7e6ebeb2
Edgar Villasmil <edgarjvh@gmail.com>
2016-12-16 11:42:44 -0400

    Initial commit

---
+++ app/src/main/java/com/villasoftgps/ebndsrrep/Frm_Login.java
@@ -0,0 +1,13 @@
+package com.villasoftgps.ebndsrrep;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Frm_Login extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
